88c7e8e13a9f55c3d5ed119a234ccba115175e3f
Svyatoslav <svyatko200311@gmail.com>
2023-07-25 17:23:37 +0300

    test

--- src/W11_june10_tusk2/Main.java
+++ src/W11_june10_tusk2/Main.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("2");
         Scanner scanner = new Scanner(System.in);
         List<Worker> workers = new ArrayList<>();
 
