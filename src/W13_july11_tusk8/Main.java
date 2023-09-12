f1ed04b3d9a03a4515e910f154230816405e4b8c
Svyatoslav <svyatko200311@gmail.com>
2023-09-12 17:00:24 +0300

    W11_june10_tusk2

---
+++ src/W13_july11_tusk8/Main.java
@@ -0,0 +1,42 @@
+package W13_july11_tusk8;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("C:\\Users\\Svyatoslav\\Desktop\\itea\\java\\COURSES\\src\\W13_july11_tusk8\\com.example.txt");
+
+        try {
+            String content = "";
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                content += line + "\n";
+            }
+            scanner.close();
+
+            System.out.println("Незмінений вміст файлу:");
+            System.out.println(content);
+
+            String replacedContent = content.replaceAll("\\b[ПпOoAa] \\b", "Java ");
+
+            FileWriter writer = new FileWriter(file);
+            writer.write(replacedContent);
+            writer.close();
+
+            System.out.println("Заміна прийменників успішно виконана!");
+
+            System.out.println("Змінений вміст файлу:");
+            System.out.println(replacedContent);
+
+        } catch (IOException e) {
+            System.out.println("Помилка при роботі з файлом:");
+            e.printStackTrace();
+        }
+    }
+}
