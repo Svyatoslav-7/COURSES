9319c44787ec9b1558557456a284a823f7c605a9
Svyatoslav <svyatko200311@gmail.com>
2023-09-12 16:46:57 +0300

    W11_june10_tusk2

---
+++ src/W13_july11_tusk7/Main.java
@@ -0,0 +1,26 @@
+package W13_july11_tusk7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\Svyatoslav\\Desktop\\itea\\java\\COURSES\\src\\W13_july11_tusk7\\com.example.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не знайдено.");
+            e.printStackTrace();
+        }
+    }
+}
+
